b327136dde5fd8d83225797ffe64f1dffa4572fe
eziquexx <waftyann@gmail.com>
2024-11-12 23:32:47 +0900

    24.11.12 지은 App.js에서 사용 안하는 import 제거

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
-import FullScreenPage from "./pages/FullScreenPage";
 import UserPage from "./pages/UserPage";
 
 //24.11.12 지은 [완료] : Router, Routes, Route 적용 테스트
