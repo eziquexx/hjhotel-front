fabc8529f0ef89a215afe90b5c56f311b1ed9365
jelee <waftyann@gmail.com>
2024-11-12 16:37:16 +0900

    App.js Route 수정, MenuPage.jsx Route 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutContent from "./component/user/about/script/AboutContent";
-import RoomContent from "./component/user/room/script/RoomContent";
-import SpecialContent from "./component/user/special/script/SpecialContent";
-import ReservationContent from "./component/user/reservation/script/ReservationContent";
-import CommunityContent from "./component/user/community/script/CommunityContent";
-import PaymentContent from "./component/user/payment/script/PaymentContent";
 import AdminPage from "./pages/AdminPage";
 import FullScreenPage from "./pages/FullScreenPage";
 
@@ -14,12 +8,6 @@ function App() {
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<FullScreenPage />} />
-        <Route path="/about" element={<AboutContent />} />
-        <Route path="/room" element={<RoomContent />} />
-        <Route path="/special" element={<SpecialContent />} />
-        <Route path="/reservation" element={<ReservationContent />} />
-        <Route path="/community" element={<CommunityContent />} />
-        <Route path="/payment" element={<PaymentContent />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,4 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "../component/common/Header";
+import AboutContent from "../component/user/about/script/AboutContent";
+import CommunityContent from "../component/user/community/script/CommunityContent";
+import PaymentContent from "../component/user/payment/script/PaymentContent";
+import ReservationContent from "../component/user/reservation/script/ReservationContent";
+import RoomContent from "../component/user/room/script/RoomContent";
+import SpecialContent from "../component/user/special/script/SpecialContent";
 
 //24.11.12 지은 [완료] : 사용자 페이지, 메뉴 테스트
 export default function MenuPage() {
@@ -6,6 +13,14 @@ export default function MenuPage() {
     <div>
       <Header />
       <h1>User Index(메인 홈)</h1>
+      <Routes>
+        <Route path="/about" element={<AboutContent />} />
+        <Route path="/room" element={<RoomContent />} />
+        <Route path="/special" element={<SpecialContent />} />
+        <Route path="/reservation" element={<ReservationContent />} />
+        <Route path="/community" element={<CommunityContent />} />
+        <Route path="/payment" element={<PaymentContent />} />
+      </Routes>
     </div>
   );
 }
