030208c3ecf3c59b121685c535d13052c26479a5
an <ssungjune0610@gmail.com>
2024-11-13 11:20:54 +0900

    24.11.13 성준 ButtonEx.jsx 에 className 추가함

--- src/component/common/ButtonEx.jsx
+++ src/component/common/ButtonEx.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 // props{ isREST, url, fetchType, data{}, isImage, src, content}
 //24.11.12 한택 [완료] : fetchApi 기능 분리 작업 완료
+//24.11.13 성준 [완료] : className 추가함
 /*
     사용 방법
 
@@ -14,9 +15,9 @@ import { useNavigate } from "react-router-dom";
     <ButtonEx id={"buttonSample"} url={"/"}>
         <p>Link</p>
     </ButtonEx>
-    
+
 */
-function ButtonEx({id, children, action=null, url = "/"}){
+function ButtonEx({id, className, children, action=null, url = "/"}){
 
     const nav = useNavigate();
 
@@ -33,7 +34,7 @@ function ButtonEx({id, children, action=null, url = "/"}){
 
     //24.10.30 HT^^ : 안쓰이는 기능 {isImage ? <img src={src} alt="" />  : <p>${content}</p>}
     return (
-        <button id={id} onClick={onClickButton}>
+        <button id={id} className={className} onClick={onClickButton}>
             {children}
         </button>
     );
