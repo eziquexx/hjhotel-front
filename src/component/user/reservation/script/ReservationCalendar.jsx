c7b6ae8a69c3fced362400e2051fa83791ea3231
Oonet96 <captaing96@gmail.com>
2024-11-14 22:57:38 +0900

    24.11.14 한택 [진행중] : 기본 캘린더 작업 완료

--- src/component/user/reservation/script/ReservationCalendar.jsx
+++ src/component/user/reservation/script/ReservationCalendar.jsx
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { setDate, setMonth, toDate } from "date-fns";
 import { useState , useEffect } from "react";
 
 function ReservationCalendar()
 {
     const nowDate = toDate(Date.now(), { timeZone: 'Asia/Seoul' });
+    const dayArr = ['일','월','화','수','목','금','토'];
 
     const [selectedYearAndMonth, setSelectedYearAndMonth] =
     useState({
@@ -30,24 +33,61 @@ function ReservationCalendar()
         const startDayOfDate = toDate(new Date(year,month,1),{timeZone : 'Asia/Seoul'});
         const lastDayOfDate = setDate(setMonth(startDayOfDate, startDayOfDate.getMonth()+1),0);
 
-        startDayOfDate.getDay();
-        lastDayOfDate.getDay();
-        
+        const timeStampArrForCurrentMonth = Array.from(
+            { length: lastDayOfDate.getDate() },
+            (_, index) => startDayOfDate.valueOf() + 24 * 60 * 60 * 1000 * index
+        );
+
+        const beforeBlankArrForCurrentMonth = Array.from(
+            {length: startDayOfDate.getDay()},
+            () => null
+        );
+
+        const nextBlankArrForCurrentMonth = Array.from(
+            {length: 6-lastDayOfDate.getDay()},
+            ()=> null
+        );
+
+        const ArrForCalendar = beforeBlankArrForCurrentMonth
+        .concat(timeStampArrForCurrentMonth, nextBlankArrForCurrentMonth);
+
+        return ArrForCalendar;
     }
 
+    
     useEffect(()=>{
         const selectedDate = toDate(selectedTimestamp , {timeZone : 'Asia/Seoul'});
-        if(selectedDate.getMonth() != selectedYearAndMonth.month){
+        if(selectedDate.getMonth() !== selectedYearAndMonth.month){
             setSelectedYearAndMonth({
                 year : selectedDate.getFullYear(),
                 month : selectedDate.getMonth()
-            })
+            });
         }
-    }) , [selectedTimestamp];
+    },[selectedTimestamp]);
+    
 
     return (
-        <div>
-
+        <div className={'container text-container'}>
+            <div className={'row row-cols-7'}>
+                {dayArr.map((day,index)=>(
+                        <div className='col' key={index}>
+                            <p>{day}</p>
+                        </div>
+                    ))
+                }
+                {getTimestampArrForCalendar(selectedYearAndMonth.year,selectedYearAndMonth.month)
+                    .map((timestamp,index)=>(
+                        <div className="col bg-primary-subtle border border-secondary" key={index}>
+                            {timestamp ? (
+                                <>
+                                    <p>{toDate(timestamp, { timeZone: 'Asia/Seoul' }).getDay()}</p>
+                                    <p>{toDate(timestamp, { timeZone: 'Asia/Seoul' }).getDate()}</p>
+                                </>
+                            ) : null}
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
